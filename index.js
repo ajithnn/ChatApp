15b3b2df44ca36ce373dc30a29fe6c1b0ad55acd
ajithnn <ajithnn20@gmail.com>
2015-01-05 21:18:07 +0530

    Errors corrected. Generic path added.

---
+++ index.js
@@ -0,0 +1,38 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http,{pingInterval:5000,pingTimeout:10000});
+var userdata = []; 
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname,'index.html'));
+});
+
+io.on('connection', function(socket){
+// ----- Chat Message Event -------------      
+  socket.on('chat message', function(msg){
+    socket.broadcast.emit('chat message', msg);
+  });
+// ----- Disconnect Message Event -------------
+  socket.on('disconnect', function(){
+	for(var i=0;i<userdata.length;i++){
+	if(userdata[i].id == socket.id)
+	{
+    var name = userdata[i].user;
+	userdata.splice(i,1);
+    io.emit('User Disconnected', name + " disconnected",userdata);
+	}
+    console.log(socket.id + " is disconnected");
+}
+});
+// ----- User Joined Event -------------    
+socket.on('User Joined', function(username){
+	userdata.push({user:username,id:socket.id});
+	io.emit('New User',username, userdata)
+});
+    
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
